49254afa9f2ce13abdc019468309cd60e82e2ec5
Mahad Zaryab <mzaryab@edu.uwaterloo.ca>
2019-01-12 18:27:28 -0500

    Most recent android studio file

---
+++ Test3/app/src/main/java/com/example/android/test3/APIThread.java
@@ -0,0 +1,41 @@
+package com.example.android.test3;
+
+import java.util.List;
+
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.api.ClarifaiResponse;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.model.ConceptModel;
+import clarifai2.dto.model.ModelVersion;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Prediction;
+import okhttp3.OkHttpClient;
+
+public class APIThread extends Thread {
+ClarifaiClient client;
+
+public APIThread(ClarifaiClient client) {
+    this.client = client;
+}
+
+
+    @Override
+    public void run() {
+
+        client = new ClarifaiBuilder("b2c657222b8e4b1ea83e97e3196a14ec")
+                .client(new OkHttpClient())
+                .buildSync();
+
+        ConceptModel model = client.getDefaultModels().generalModel();
+       // ModelVersion modelVersion = model.getVersionByID("the-version").executeSync().get();
+
+        ClarifaiResponse<List<ClarifaiOutput<Prediction>>> response = client.predict(model.id())
+                .withInputs(ClarifaiInput.forImage("https://loopnewslive.blob.core.windows.net/liveimage/sites/default/files/2018-08/SldlWkSDOg.jpg"))
+                .executeSync();
+        System.out.println(response.get();
+
+
+
+    }
+}
---
+++ Test3/app/src/main/java/com/example/android/test3/MainActivity.java
@@ -0,0 +1,77 @@
+package com.example.android.test3;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.List;
+
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.api.ClarifaiResponse;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.model.ConceptModel;
+import clarifai2.dto.model.ModelVersion;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Prediction;
+import okhttp3.OkHttpClient;
+
+
+public class MainActivity extends AppCompatActivity {
+    public ClarifaiClient client;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        System.out.println("Initialized");
+        setSupportActionBar(toolbar);
+
+
+
+        Thread thread = new APIThread(client);
+        thread.start();
+
+
+
+
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
